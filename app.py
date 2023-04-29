cc653ff531281f95b1dd0147b50e2d713b293db0
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2023-04-29 15:24:17 -0300

    Added description and character limit

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def get_itinerary(dest_name, length_of_stay):
     # Extract the response text
 
     itinerary = response.choices[0].text.strip()
-
+    
     # Inserting a line break after each day
 
     itinerary = itinerary.replace("Day:", ":\nDay")
@@ -40,10 +40,31 @@ def get_itinerary(dest_name, length_of_stay):
 
 st.title("Travel Advisory")
 
+# Defining the footer content and the link to the source code
+
+descript = """Made by Prayag Shah with ❤️. Check out the source code at [GitHub](https://github.com/prayagnshah/travel-advisory)"""
+
+# Add the footer to the app
+st.markdown(descript, unsafe_allow_html=True)
+
+# Set the maximum number of words in the destination name
+MAX_WORDS = 10
+
 # Get user input
 
 destination_name = st.text_input("Enter your destination")
-length_of_stay = st.slider("Enter the length of your stay", 0, 10)
+
+# Count the number of words in the input
+
+num_words = len(destination_name.split())
+
+# Show a warning message if the number of words exceeds the limit
+
+if num_words > MAX_WORDS:
+    st.warning("The destination name should be less than 10 words")
+    st.stop()
+
+length_of_stay = st.slider("Enter the length of your stay", 1, 10)
 
 # Generate itinerary and show loading message
 
