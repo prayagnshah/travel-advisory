96ab0f292bd56057353866047315528850eb3ac2
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2023-04-30 10:16:50 -0300

    minor variable name change

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import redis
 import requests
 from dotenv import load_dotenv
 
-# Calling key from environment variables
-
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -15,19 +13,17 @@ host = os.environ.get('REDIS_HOST')
 port = os.environ.get('REDIS_PORT')
 password = os.environ.get('REDIS_PASSWORD')
 
+redis_client = redis.Redis(host=host, port=port, password=password, decode_responses=True)
 
-def get_itinerary(dest_name, length_of_stay):
+
+def get_itinerary(destination_name, length_of_stay):
     
-    # Reading redis credentials from environment variables
-    redis_client = redis.Redis(host=host, port=port, password=password, decode_responses=True)
-    
-    # Creating a key for the destination name and length of stay
-    key = f"{dest_name}_{length_of_stay}"
+    itinerary_key = f"{destination_name}_{length_of_stay}"
     placeholder = st.empty()
     
-    # If same key is called then return the value from redis
-    if redis_client.exists(key):
-        itinerary = redis_client.get(key)
+    
+    if redis_client.exists(itinerary_key):
+        itinerary = redis_client.get(itinerary_key)
         placeholder.text("")
         st.success(itinerary)
         st.stop()
@@ -40,11 +36,13 @@ def get_itinerary(dest_name, length_of_stay):
     if redis_client.exists(user_ip):
         request_count = int(redis_client.get(user_ip))
         
-        if request_count > 10:
+        RATE_LIMIT_PER_DAY = 10
+        if request_count > RATE_LIMIT_PER_DAY:
             st.write("You have exceeded the maximum number of requests. Please try again tomorrow or reach out to me to increase rate-limit.")
             st.stop()
     else:
-        redis_client.set(user_ip, 0, ex=86400)
+        SECONDS_IN_A_DAY = 86400
+        redis_client.set(user_ip, 0, ex=SECONDS_IN_A_DAY)
     
     # Incrementing the request count for the IP address
     redis_client.incr(user_ip)
@@ -53,11 +51,11 @@ def get_itinerary(dest_name, length_of_stay):
     response = openai.Completion.create(
         engine="text-davinci-003",  # Which GPT-3 engine to use
         prompt="Can you recommend a " + str(length_of_stay) + \
-        "day itinerary for " + str(dest_name) + "?" + \
-        "in detail ? And they should be in format day1, day2, etc",  # The input text
+        "day itinerary for " + str(destination_name) + "?" + \
+        "in detail ? And they should be in format Day 1, Day 2, etc",  # The input text
         temperature=0.2,  # How creative the response should be
         max_tokens=1024,  # Maximum length of the response
-        n=2,  # How many responses to generate
+        n=1,  # How many responses to generate
         stop=None  # Text to stop generation at (optional)
     )
 
@@ -65,25 +63,25 @@ def get_itinerary(dest_name, length_of_stay):
     itinerary = response.choices[0].text.strip()
     
     # Inserting a line break after each day
-    itinerary = itinerary.replace("Day:", "\nDay")
+    itinerary = itinerary.replace(":", "\n")
     
     # Storing the key and value in redis for 1 week
-    redis_client.set(key, itinerary, ex=604800)
+    # redis_client.set(itinerary_key, itinerary, ex=604800)
 
     return itinerary
 
 # Set up the app name
 st.title("Travel Advisory")
 
-# Defining the footer content and the link to the source code
+# Defining the description and link to the source code
 
-descript = """Made by Prayag Shah with ❤️. Check out the source code at [GitHub](https://github.com/prayagnshah/travel-advisory). <br> Feel free to reach out on [Linkedin](https://www.linkedin.com/in/prayag-shah/) if you have any questions."""
+description = """Made by Prayag Shah with ❤️. Check out the source code at [GitHub](https://github.com/prayagnshah/travel-advisory). <br> Feel free to reach out on [Linkedin](https://www.linkedin.com/in/prayag-shah/) if you have any questions."""
 
 # Add the footer to the app
-st.markdown(descript, unsafe_allow_html=True)
+st.markdown(description, unsafe_allow_html=True)
 
 # Set the maximum number of words in the destination name
-max_words= 3
+MAX_WORDS= 3
 
 # Get user input
 destination_name = st.text_input("Enter your destination")
@@ -92,7 +90,7 @@ destination_name = st.text_input("Enter your destination")
 num_words = len(destination_name.strip().split())
 
 # Show a warning message if the number of words exceeds the limit
-if num_words > max_words:
+if num_words > MAX_WORDS:
     st.warning("The destination name should be less than 3 words")
     st.stop()
 
