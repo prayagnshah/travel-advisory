2e731b85d4ed5f98164660f7fe871cd2e0ca7729
prayagnshah <34435248+prayagnshah@users.noreply.github.com>
2023-04-30 13:35:38 -0300

    converted code into functions for more readable and maintanable

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import requests
 import random
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -18,69 +19,109 @@ redis_client = redis.Redis(host=host, port=port, password=password, decode_respo
 
 
 def get_itinerary(destination_name, type_of_trip, length_of_stay):
+    """
+    Will store the itinerary in Redis for 7 days and checking the rate limit of the user
+    """
+    itinerary_key = get_itinerary_key(destination_name, type_of_trip, length_of_stay)
+    itinerary = get_itinerary_from_redis(itinerary_key)
     
-    itinerary_key = f"{destination_name}_{length_of_stay}_{type_of_trip}"
+    if itinerary:
+        return itinerary
     
+    user_ip = get_user_ip()
+    check_rate_limit(user_ip)
+    increment_request_count(user_ip)
     
-    if redis_client.exists(itinerary_key):
-        itinerary = redis_client.get(itinerary_key)
-        
-        if itinerary:
-            return itinerary
-        
+    itinerary = call_openai_api(destination_name, type_of_trip, length_of_stay)
+    itinerary = format_itinerary(itinerary)
     
-    # Getting the IP address of the user
-    user_ip = requests.get('https://api.ipify.org').text
-    
-    # Checking if IP address exists in database
-    if redis_client.exists(user_ip):
-        request_count = int(redis_client.get(user_ip))
-        
-        RATE_LIMIT_PER_DAY = 10
-        if request_count > RATE_LIMIT_PER_DAY:
-            st.write("You have exceeded the maximum number of requests. Please try again tomorrow or reach out to me to increase rate-limit.")
-            st.stop()
-    else:
-        SECONDS_IN_A_DAY = 86400
-        redis_client.set(user_ip, 0, ex=SECONDS_IN_A_DAY)
-    
-    prompt = "Can you recommend a " + str(length_of_stay) + "day itinerary for " + str(destination_name) + "?" + "in detail ? And they should be in format Day 1, Day 2, etc" + "Type of trip should be based on" + str(type_of_trip) + "trip"
-
-    # Call the OpenAI API
-    response = openai.Completion.create(
-        engine="text-davinci-003",  # Which GPT-3 engine to use
-        prompt=prompt,  # The input text
-        temperature=0.2,  # How creative the response should be
-        max_tokens=1024,  # Maximum length of the response
-        n=1,  # How many responses to generate
-        stop=None  # Text to stop generation at (optional)
-    )
-
-    itinerary = response.choices[0].text.strip()
-    
-    itinerary = itinerary.replace(":", "\n")
-    
-    # Incrementing the request count for the IP address
-    redis_client.incr(user_ip)
-    
-    # Storing the key and value in redis for 1 week
     redis_client.set(itinerary_key, itinerary, ex=604800)
-
+    
     return itinerary
 
-# title
+
+def get_itinerary_key(destination_name, type_of_trip, length_of_stay):
+    """
+    Storing the input values to avoid the future API calls
+    """
+    return f"{destination_name}_{length_of_stay}_{type_of_trip}"
+
+
+def get_itinerary_from_redis(itinerary_key):
+    """
+    If the itinerary exists in Redis database, returns it directly without making the API call
+    """
+    if redis_client.exists(itinerary_key):
+        itinerary = redis_client.get(itinerary_key)
+        if itinerary:
+            return itinerary
+
+
+def get_user_ip():
+    """
+    Getting the IP address of the user
+    """
+    return requests.get('https://api.ipify.org').text
+
+
+def check_rate_limit(user_ip):
+    """
+    Checking the count of IP address in Redis database and if it exceeds the limit, stops the execution
+    """
+    if redis_client.exists(user_ip):
+        request_count = int(redis_client.get(user_ip))
+        RATE_LIMIT_PER_DAY = 10
+        if request_count > RATE_LIMIT_PER_DAY:
+            st.write("You have exceeded the maximum number of requests. Please try again tomorrow or reach out to me on [Linkedin](https://www.linkedin.com/in/prayag-shah/) to increase rate-limit.")
+            st.stop()
+
+
+def increment_request_count(user_ip):
+    """
+    IP will get reset after 24 hours
+    """
+    SECONDS_IN_A_DAY = 86400
+    redis_client.incr(user_ip)
+    redis_client.expire(user_ip, SECONDS_IN_A_DAY)
+
+
+def call_openai_api(destination_name, type_of_trip, length_of_stay):
+    """
+    Making the API call to OpenAI
+    """
+    prompt = f"Can you recommend a {length_of_stay} day itinerary for {destination_name}? In detail? And they should be in format Day 1, Day 2, etc. Type of trip should be based on {type_of_trip} trip."
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.2,
+        max_tokens=1024,
+        n=1,
+        stop=None
+    )
+
+    return response.choices[0].text.strip()
+
+
+def format_itinerary(itinerary):
+    """
+    Showing Day 1, Day 2, etc. in a new line
+    """
+    return itinerary.replace(":", "\n")
+        
+
+
+# Title
 st.title("Travel Advisory")
 
-# Defining the description and link to the source code
-
+# Description
 description = """Made by Prayag Shah with ❤️. Check out the source code at [GitHub](https://github.com/prayagnshah/travel-advisory). <br> Feel free to reach out on [Linkedin](https://www.linkedin.com/in/prayag-shah/) if you have any questions."""
-
 st.markdown(description, unsafe_allow_html=True)
 
 # Set the maximum number of words in the destination name
 MAX_WORDS= 3
 
-# Get user input
+# User's input
 destination_name = st.text_input("Enter your destination")
 
 num_words = len(destination_name.strip().split())
